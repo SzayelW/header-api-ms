cc44c8650ddfc7bdfa3a8f31233155eaed39502f
Andres <andres70p32@gmail.com>
2017-04-09 07:14:57 +0000

    project working

--- server.js
+++ server.js
@@ -3,12 +3,24 @@ var app = express()
 
 app.get('/', (req, res)=>{
     var ipadd = req.headers['x-forwarded-for']
-    var lang = req.headers['accept-language']
-    var soft = req.headers['user-agent']
+    var lang = getLang(req.headers['accept-language'])
+    var soft = getUserAgent(req.headers['user-agent'])
     var response = { ipaddress: ipadd, language : lang, software: soft}
     res.setHeader('Content-Type', 'application/json')
     res.send(response)
 });
+
+function getLang(language){
+    var re = /([a-zA-Z_-]+),/gi
+    var array = re.exec(language)
+    return array[1]
+}
+
+function getUserAgent(userAgent){
+    var reg = /\(([^)]+)\)/gi
+    var array = reg.exec(userAgent)
+    return array[1]
+}
 
 app.listen(process.env.PORT || 8080, ()=>{
     console.log("running on port: " + process.env.PORT || "8080")
