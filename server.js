59105ec69bb41f638d57ea31349f2ca8482d0b8a
Andres <andres70p32@gmail.com>
2017-04-09 05:46:18 +0000

    working headers

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ var express = require('express')
 var app = express()
 
 app.get('/', (req, res)=>{
-    console.log("initial")
+    var ipadd = req.headers['x-forwarded-for']
+    var lang = req.headers['accept-language']
+    var soft = req.headers['user-agent']
+    var response = { ipaddress: ipadd, language : lang, software: soft}
+    res.setHeader('Content-Type', 'application/json')
+    res.send(response)
 });
 
 app.listen(process.env.PORT || 8080, ()=>{
